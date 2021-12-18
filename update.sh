513d0d8b57dbc38077017bac0cb97d948e932c43
d4m1ts <damit5@protonmail.com>
2021-12-18 21:51:33 +0800

    21-12-18 21:51:33 更新book.json

--- update.sh
+++ update.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-git add *
-git commit -m "`date +'%y-%m-%d %H:%M:%S'` $1"
-git push -u origin master
+if [ -n "$1" ]
+then
+    git add *
+    git commit -m "`date +'%y-%m-%d %H:%M:%S'` $1"
+    git push -u origin master
+
+else
+    echo "必须输入一个参数！！"
+fi
