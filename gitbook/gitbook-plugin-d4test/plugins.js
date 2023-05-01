829b428af89571a8dfe0cf9ca0f88e5b87e54bbd
d4m1ts <damit5@protonmail.com>
2023-05-01 22:36:39 +0800

    23-05-01 22:36:39 更新

--- gitbook/gitbook-plugin-d4test/plugins.js
+++ gitbook/gitbook-plugin-d4test/plugins.js
@@ -2,28 +2,32 @@ require(["gitbook"], function(gitbook) {
 
     // start事件
     gitbook.events.bind("start", function(e, pluginConfig) {
-        // 拿到配置文件
-        var config = pluginConfig.d4test || {};
-        console.log(pluginConfig);
-        // token不为空
-        if (!config.token) {
-            throw "Need to option 'token' for Google ads plugin";
-        }
         // 获取<head>标签
         let head = document.getElementsByTagName('head')[0];
         // 创建<script>元素
         let script = document.createElement('script');
         script.async = true;
-        script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=" + config.token;
+        script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9261110029983076";
         script.crossOrigin = "anonymous";
 
         // 将<script>元素添加到<head>标签中
         head.appendChild(script);
+        console.log("google ads test");
     });
 
     // page.change事件
     gitbook.events.bind("page.change", function() {
-        console.log("test");
+        // 获取<head>标签
+        let head = document.getElementsByTagName('head')[0];
+        // 创建<script>元素
+        let script = document.createElement('script');
+        script.async = true;
+        script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9261110029983076";
+        script.crossOrigin = "anonymous";
+
+        // 将<script>元素添加到<head>标签中
+        head.appendChild(script);
+        console.log("google ads test");
     });
 
 });
