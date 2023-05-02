10860f44d51c7b481fb2a0e452dc5b440d647317
d4m1ts <damit5@protonmail.com>
2023-05-02 12:22:32 +0800

    23-05-02 12:22:32 更新渗透测试介绍

--- gitbook/gitbook-plugin-d4test/baidu_tongji.js
+++ gitbook/gitbook-plugin-d4test/baidu_tongji.js
@@ -7,17 +7,11 @@ require(["gitbook"], function(gitbook) {
         script.type = 'text/javascript';
         script.innerHTML = "var _hmt = _hmt || [];(function() {var hm = document.createElement('script');hm.src = 'https://hm.baidu.com/hm.js?3675030a9721336abea8cd0e46b3f76b';var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(hm, s);})();";
         head.appendChild(script);
-        console.log("test_baidu_tongji");
     });
 
     // page.change事件
     gitbook.events.bind("page.change", function() {
-        var head = document.getElementsByTagName('head')[0];
-        var script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.innerHTML = "var _hmt = _hmt || [];(function() {var hm = document.createElement('script');hm.src = 'https://hm.baidu.com/hm.js?3675030a9721336abea8cd0e46b3f76b';var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(hm, s);})();";
-        head.appendChild(script);
-        console.log("test_baidu_tongji");
+        _hmt.push(['_trackEvent', location.pathname, 'page.change', '', '']);
     });
 
 });
